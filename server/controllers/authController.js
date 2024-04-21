b8f65abcf671aa45c4edfe2efa9b6bf353304cd2
DelanoMarcell <delanomartin14@gmail.com>
2024-04-21 22:16:51 +0200

    Fix formatting in authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -203,7 +203,7 @@ exports.forgetPassword = async (req, res) => {
       to: user.email,
       subject: "Password Reset Request",
       text: `
-        Hello, ${user.name},
+        Hello ${user.name},
 
         We received a request to reset your password. If you did not make this request, please ignore this email.
 
@@ -301,7 +301,7 @@ exports.resetPassword = async (req, res) => {
       to: user.email,
       subject: "Password Reset Successful",
       text: `
-        Hello, ${user.name},
+        Hello ${user.name},
 
         Your password has been successfully reset.
 

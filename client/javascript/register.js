ecfe625f13cc03a59cdad132bd4f40b36763bcad
Danish Saleem <saleemdf99@gmail.com>
2024-04-21 23:38:10 +0200

    updated error messages UI

--- client/javascript/register.js
+++ client/javascript/register.js
@@ -1,86 +1,91 @@
-var submitButton = document.getElementById('submitButton');
+var submitButton = document.getElementById("submitButton");
 
-submitButton.addEventListener('click', function(event) {
-    event.preventDefault();
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-   console.log('Submit button clicked');
+  console.log("Submit button clicked");
 
-   // Validate form fields
-   var fname = document.getElementById('fname').value;
-   var lname = document.getElementById('lname').value;
-   var email = document.getElementById('email').value;
-   var password = document.getElementById('psw').value;
-   var confirmPassword = document.getElementById('psw-confirm').value;
+  // Validate form fields
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("psw").value;
+  var confirmPassword = document.getElementById("psw-confirm").value;
 
-   if (!fname || !lname || !email || !password || !confirmPassword || fname === "" || lname === "" || email === "" || password === "" || confirmPassword === "") {
-    var alert = document.getElementById('alert');
-    alert.style.display = 'block';
+  if (
+    !fname ||
+    !lname ||
+    !email ||
+    !password ||
+    !confirmPassword ||
+    fname === "" ||
+    lname === "" ||
+    email === "" ||
+    password === "" ||
+    confirmPassword === ""
+  ) {
+    var alert = document.getElementById("alert");
+    alert.style.display = "block";
     alert.innerText = "Please fill in all fields";
     return;
-   }
+  }
 
+  //show loader while waiting for response
+  var loader = document.getElementById("loader");
+  loader.style.display = "flex";
 
-   //show loader while waiting for response
-   var loader = document.getElementById('loader');
-   loader.style.display = 'flex';
+  // post request to register user
+  fetch("/api/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: document.getElementById("fname").value,
+      surname: document.getElementById("lname").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("psw").value,
+      confirmPassword: document.getElementById("psw-confirm").value,
+      role: document.getElementById("acc-type").value,
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      //hide loader when response is received
+      loader.style.display = "none";
 
+      if (data.message === "User registered successfully") {
+        //Show success message
+        var alert = document.getElementById("alert");
+        alert.style.display = "block";
+        alert.innerText = "User registered successfully";
+        alert.className =
+          "bg-green-100 border hidden border-green-400 text-green-700 px-2 py-2 rounded-2xl text-center mb-[4%]";
 
-   // post request to register user
-   fetch('/api/auth/register', {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-         name: document.getElementById('fname').value,
-         surname: document.getElementById('lname').value,
-         email: document.getElementById('email').value,
-         password: document.getElementById('psw').value,
-         confirmPassword: document.getElementById('psw-confirm').value,
-         role: document.getElementById('acc-type').value,
-      }),
-   })
-      .then((response) => {
-         return response.json();
-      })
-      .then((data) => {
-         console.log(data);
-         //hide loader when response is received
-         loader.style.display = 'none';
+        //clear form fields
+        document.getElementById("fname").value = "";
+        document.getElementById("lname").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("psw").value = "";
+        document.getElementById("psw-confirm").value = "";
 
-       
-         if(data.message === "User registered successfully"){
-            //Show success message 
-            var alert = document.getElementById('alert');
-            alert.style.display = 'block';
-            alert.innerText = "User registered successfully";
-
-            //clear form fields
-            document.getElementById('fname').value = '';
-            document.getElementById('lname').value = '';
-            document.getElementById('email').value = '';
-            document.getElementById('psw').value = '';
-            document.getElementById('psw-confirm').value = '';
-
-            // redirect to login page
-            setTimeout(() => {
-               window.location.href = '/login';
-            }, 5000);
-           
-
-         }else if(data.error){
-            //Show error message
-            var alert = document.getElementById('alert');
-            alert.style.display = 'block';
-            alert.innerText = data.error;
-         }
-         
-      })
-      .catch((error) => {
-         console.log('Error:', error);
-         loader.style.display = 'none';
-         alert('An error occurred. Please try again');
-      });
-
-
+        // redirect to login page
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 5000);
+      } else if (data.error) {
+        //Show error message
+        var alert = document.getElementById("alert");
+        alert.style.display = "block";
+        alert.innerText = data.error;
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+      loader.style.display = "none";
+      alert("An error occurred. Please try again");
+    });
 });

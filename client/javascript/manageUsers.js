f94a43f6c29cdb99922d796fff924f88fcacd796
Danish Saleem <saleemdf99@gmail.com>
2024-04-28 23:48:43 +0200

    Updated UI for user management + functionality

--- client/javascript/manageUsers.js
+++ client/javascript/manageUsers.js
@@ -1,32 +1,51 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function getRoles(role) {
-    let roles = ["Admin", "Staff", "Resident"];
+let logo = document.getElementById("logo");
+const mainLoader = document.getElementById("mainLoader");
+const userInfo = document.getElementById("userInfo");
 
-    //retunr roles array without role
-    return roles.filter((r) => r !== role);
-  }
+logo.addEventListener("click", () => {
+  window.location.href = "/admin";
+});
 
-  fetch("/api/users/getAllUsers")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      //do stuff
-      console.log(data.users);
+function getRoles(role) {
+  let roles = ["Admin", "Staff", "Resident"];
 
-      let users = data.users;
+  //retunr roles array without role
+  return roles.filter((r) => r !== role);
+}
 
-      let table = document.getElementById("usersTable");
-      let tableBody = document.getElementById("tbody");
+mainLoader.classList.remove("hidden");
+userInfo.classList.add("hidden");
 
-      let updateImage = "../assets/edit.png";
-      let deleteImage = "../assets/delete.svg";
+fetch("/api/users/getAllUsers")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    //do stuff
+    //console.log(data.users);
 
-      users.forEach((user) => {
-        let otherRoles = getRoles(user.role);
+    setTimeout(function () {
+      mainLoader.classList.add("hidden");
+      userInfo.classList.remove("hidden");
+    }, 1500);
 
-        tableBody.innerHTML += `
-                    <tr class="bg-white border-b hover:bg-gray-200">
+    let users = data.users;
+
+    let table = document.getElementById("usersTable");
+    let tableBody = document.getElementById("tbody");
+
+    let updateImage = "../assets/edit.png";
+    let deleteImage = "../assets/delete.svg";
+    let checkImage = "../assets/check.svg";
+    let crossImage = "../assets/cross.svg";
+
+    let i = 0;
+
+    users.forEach((user) => {
+      let otherRoles = getRoles(user.role);
+
+      tableBody.innerHTML += `
+                    <tr id="row_${user.userCode}" class="bg-white border-b hover:bg-gray-200 ">
                     <th scope="row" class="px-3 py-1 font-medium text-gray-900 text-center ">
                         ${user.name} ${user.surname}
                     </th>
@@ -38,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     </td>
                     <td class="px-5 py-1 text-center w-[15%]">
                         <select class="py-1.5 text-center text-xs rounded-3xl bg-gray-100 w-[100%] border-black" name=""
-                            id="">
+                            id="select_${user.userCode}">
                             <option value="${user.role}">${user.role}</option>
                             <option value="${otherRoles[0]}">${otherRoles[0]}</option>
                             <option value="${otherRoles[1]}">${otherRoles[1]}</option>        
@@ -48,20 +67,157 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     <td class="px-6 py-1 text-center w-[15%] mx-auto">
                         <div class="flex justify-center">
-                        <img src="${updateImage}" class="w-6 h-6">
+                        <img id="update_${user.userCode}" src="${updateImage}" class="w-6 h-6 cursor-pointer">
+                         <svg aria-hidden="true"
+                        class="w-6 h-6 ml-2 hidden text-green-200 animate-spin dark:text-gray-600 fill-[#004621]"
+                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg" id="updateLoader_${user.userCode}">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor" />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill" />
+                        </svg>
+                        <img id="checkUpdate_${user.userCode}" src="${checkImage}" class="w-6 h-6 ml-2 hidden">
+                        <img id="crossUpdate_${user.userCode}" src="${crossImage}" class="w-6 h-6 ml-2 hidden">
+
+                        
                         </div>
                     </td>
 
                     <td class="px-6 py-1 text-center w-[15%] ">
                         <div class="flex justify-center" >
-                            <img src="${deleteImage}" class="w-6 h-6"> 
+                            <img id="delete_${user.userCode}" src="${deleteImage}" class="w-6 h-6 cursor-pointer"> 
+                        <svg aria-hidden="true"
+                        class="w-6 h-6 ml-2 hidden text-green-200 animate-spin dark:text-gray-600 fill-[#004621]"
+                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg" id="loader_${user.userCode}">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor" />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill" />
+                        </svg>
+                        <img id="crossDelete_${user.userCode}" src="${crossImage}" class="w-6 h-6 ml-2 hidden">
+
                         </div>
        
                     </td>
                 </tr>`;
-      });
-    })
-    .catch((error) => {
-      console.log("Error:", error);
     });
-});
+
+    document.getElementById("tbody").addEventListener("click", (event) => {
+      if (event.target && event.target.id.startsWith("update_")) {
+        event.preventDefault();
+
+        // Extract the user code from the update button's id
+        const userCode = event.target.id.split("_")[1];
+
+        // Get the new role from the corresponding select element
+        const newRole = document.getElementById(`select_${userCode}`).value;
+
+        //show loader
+        document
+          .getElementById(`updateLoader_${userCode}`)
+          .classList.remove("hidden");
+
+        // Send the update request
+        fetch("/api/users/manageUsers", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userCode: userCode,
+            role: newRole,
+            del: false,
+          }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            //hide loader
+            document
+              .getElementById(`updateLoader_${userCode}`)
+              .classList.add("hidden");
+            //show check for 3 seconds
+            document
+              .getElementById(`checkUpdate_${userCode}`)
+              .classList.remove("hidden");
+            setTimeout(function () {
+              document
+                .getElementById(`checkUpdate_${userCode}`)
+                .classList.add("hidden");
+            }, 1500);
+            console.log(data);
+          })
+          .catch((error) => {
+            //hide loader
+            document
+              .getElementById(`updateLoader_${userCode}`)
+              .classList.add("hidden");
+            //show cross for 3 seconds
+            document
+              .getElementById(`crossUpdate_${userCode}`)
+              .classList.remove("hidden");
+            setTimeout(function () {
+              document
+                .getElementById(`crossUpdate_${userCode}`)
+                .classList.add("hidden");
+            }, 1500);
+            console.log("Error:", error);
+          });
+      } else if (event.target && event.target.id.startsWith("delete_")) {
+        event.preventDefault();
+
+        // Extract the user code from the delete button's id
+        const userCode = event.target.id.split("_")[1];
+
+        // Show the loader
+        document
+          .getElementById(`loader_${userCode}`)
+          .classList.remove("hidden");
+
+        // Send the delete request
+        fetch("/api/users/manageUsers", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userCode: userCode,
+            role: "",
+            del: true,
+          }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // Hide the loader
+            document
+              .getElementById(`loader_${userCode}`)
+              .classList.add("hidden");
+            console.log(data);
+            //delete tr with id row_userCode
+            document.getElementById(`row_${userCode}`).remove();
+          })
+          .catch((error) => {
+            // Hide the loader
+            document
+              .getElementById(`loader_${userCode}`)
+              .classList.add("hidden");
+            //show cross for 3 seconds
+            document
+              .getElementById(`crossDelete_${userCode}`)
+              .classList.remove("hidden");
+            setTimeout(function () {
+              document
+                .getElementById(`crossDelete_${userCode}`)
+                .classList.add("hidden");
+            }, 1500);
+            console.log("Error:", error);
+          });
+      }
+    });
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -108,15 +108,6 @@ exports.getUserDetails = async (req, res) => {
 
 exports.manageUsers = async (req, res) => {
   try {
-    //maybe instead of having differnt endpoints for like deleting and updating we can have one endpoint that does all of that, but we
-    //can have a field in the request body that tells us what we want to do, like delete, update, etc.
-    //So for example. if the request body has a field called action that is set to delete, we can delete the user.
-    //If the action field is set to update, we can update the user. So we send back to backend
-    // {user : userCode , actions: [role, email, name, delete]}, only role sent for now, but in future if we wanna
-    //update email or name, we can add that to the array of actions. we know based on indexing
-
-    //{user: userCode, actions: [role, email, name, delete], del: true}
-
     const { userCode, role, del } = req.body;
 
     //Check if delete is true, if it is, delete the user
@@ -128,10 +119,12 @@ exports.manageUsers = async (req, res) => {
     //Update user permissions, will only run if delete is not true
     const userToUpdate = await User.findOne({ userCode });
 
-    userToUpdate.role = actions[0];
+    userToUpdate.role = role;
     await userToUpdate.save();
 
-    res.status(200).json({ message: "User permissions updated" });
+    res
+      .status(200)
+      .json({ message: "User permissions updated", user: userToUpdate });
   } catch (error) {
     console.log("Error managing user:", error);
     res.status(500).json({ error: "Internal server error." });

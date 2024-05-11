26fa7b70fbfdec76fc08333e63a3beb58185bd0b
Qais Alsaleh <qaisalsaleh251@gmail.com>
2024-05-11 21:10:36 +0200

    Codecov implementation

---
+++ server/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    collectCoverage: true,
+    coverageReporters: ['text', 'cobertura'],
+    passWithNoTests: true,
+    verbose: true,
+    testEnvironment: 'node',
+    detectOpenHandles: true,
+    errorOnDeprecated: true,
+}

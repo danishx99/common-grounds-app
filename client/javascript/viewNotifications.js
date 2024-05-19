e6641bb775c1db147e93f286394968f8117e3f0c
DelanoMarcell <delanomartin14@gmail.com>
2024-05-19 21:01:24 +0200

    Refactor viewNotifications.js and output.css

--- client/javascript/viewNotifications.js
+++ client/javascript/viewNotifications.js
@@ -1,75 +1,9 @@
 let logo = document.getElementById("logo");
 const mainLoader = document.getElementById("mainLoader");
 const finesInfo = document.getElementById("finesInfo");
-const redCircle = document.getElementById("redCircle");
-const notificationList = document.getElementById("notificationList");
 
 
 
-
-
-const fetchNotifications = async () => {
-    
-  //get request to /api/fines/hasUnreadFines
-  await fetch("/api/fines/hasUnreadFines")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      if (data.unreadFines > 0) {
-        redCircle.style.display = "block";
-        notificationList.innerHTML += `<li><a id="notificationFine" class="block px-4 py-2 hover:bg-gray-100">You have been issued ${data.unreadFines} new fines.</a></li>`;
-        document
-          .getElementById("notificationFine")
-          .addEventListener("click", function () {
-            window.location.href = "/resident/viewFines";
-          });
-      }
-      if (data.error) {
-        notificationList.innerHTML += `<li><a class="block px-4 py-2 hover:bg-gray-100">An error occurred while fetching fines.</a></li>`;
-      }
-    })
-    .catch((error) => {
-      //append error message to notification list
-      notificationList.innerHTML += `<li><a class="block px-4 py-2 hover:bg-gray-100">An error occurred while fetching fines.</a></li>`;
-      console.log("Error:", error);
-    });
-
-  //get request to /api/notifications/getUnreadNotifications
-  await fetch("/api/notifications/getUnreadNotifications")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-    if (data.unreadNotifications > 0) {
-      redCircle.style.display = "block";
-      notificationList.innerHTML += `<li><a id="notificationNotification" class="block px-4 py-2 hover:bg-gray-100"> You have ${data.unreadNotifications} unread notifications.</a></li>`;
-        document.getElementById("notificationNotification")
-        .addEventListener("click", function () {
-          window.location.href = "/resident/viewNotifications";
-        });
-    }
-    if (data.error) {
-      notificationList.innerHTML += `<li><a class="block px-4 py-2 hover:bg-gray-100">An error occurred while fetching your notifications.</a></li>`;
-    }
-  })
-  .catch((error) => {
-    //append error message to notification list
-    notificationList.innerHTML += `<li><a class="block px-4 py-2 hover:bg-gray-100">An error occurred while fetching your notifications.</a></li>`;
-    console.log("Error:", error);
-  });
-
-
-  //check if notificationList is empty
-  if (notificationList.innerHTML.trim() === "") {
-    notificationList.innerHTML += `<li><a class="block px-4 py-2 cursor-not-allowed">You have no new notifications.</a></li>`;
-  }
-};
-
-  
-fetchNotifications();
 
 
 

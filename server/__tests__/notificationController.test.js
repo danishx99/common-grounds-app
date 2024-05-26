ee9670e25f3f561f53c6f9a45c9c31ad9939e320
Danish Saleem <saleemdf99@gmail.com>
2024-05-26 22:09:50 +0200

    updated notification testing

--- server/__tests__/notificationController.test.js
+++ server/__tests__/notificationController.test.js
@@ -1,32 +1,32 @@
-const notificationController = require('../controllers/notificationController');
-const Notification = require('../models/Notification');
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
+const notificationController = require("../controllers/notificationController");
+const Notification = require("../models/Notification");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
-jest.mock('../models/Notification');
-jest.mock('../models/User');
-jest.mock('jsonwebtoken');
+jest.mock("../models/Notification");
+jest.mock("../models/User");
+jest.mock("jsonwebtoken");
 beforeEach(() => {
   // Mock console.error
-  jest.spyOn(console, 'error').mockImplementation(() => {});
+  jest.spyOn(console, "error").mockImplementation(() => {});
 });
 
 afterEach(() => {
   // Restore the original console.error function
   console.error.mockRestore();
 });
-describe('sendNotification', () => {
-  it('should send notification successfully', async () => {
+describe("sendNotification", () => {
+  it("should send notification successfully", async () => {
     const req = {
       cookies: {
-        token: 'token',
+        token: "token",
       },
       body: {
-        title: 'Notification Title',
-        description: 'Notification Description',
+        title: "Notification Title",
+        description: "Notification Description",
       },
     };
-    const verified = { userCode: '123' };
+    const verified = { userCode: "123" };
     jwt.verify.mockReturnValue(verified);
     const saveMock = jest.fn().mockResolvedValue({});
     Notification.mockImplementationOnce(() => ({
@@ -42,18 +42,18 @@ describe('sendNotification', () => {
     expect(saveMock).toHaveBeenCalled();
     expect(res.status).toHaveBeenCalledWith(201);
     expect(res.json).toHaveBeenCalledWith({
-      message: 'Notification sent successfully',
+      message: "Notification sent successfully",
     });
   });
 
-  it('should handle error when sending notification', async () => {
+  it("should handle error when sending notification", async () => {
     const req = {
       cookies: {
-        token: 'token',
+        token: "token",
       },
       body: {
-        title: 'Notification Title',
-        description: 'Notification Description',
+        title: "Notification Title",
+        description: "Notification Description",
       },
     };
     const res = {
@@ -62,23 +62,23 @@ describe('sendNotification', () => {
     };
 
     Notification.mockImplementationOnce(() => {
-      throw new Error('Error sending notification');
+      throw new Error("Error sending notification");
     });
 
     await notificationController.sendNotification(req, res);
 
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({
-      error: 'Error sending notification',
+      error: "Error sending notification",
     });
   });
 });
 
-describe('getNotifications', () => {
-  it('should fetch notifications successfully', async () => {
+describe("getNotifications", () => {
+  it("should fetch notifications successfully", async () => {
     const req = {
       cookies: {
-        token: 'token',
+        token: "token",
       },
     };
     const res = {
@@ -87,95 +87,96 @@ describe('getNotifications', () => {
     };
     const notifications = [
       {
-        _id: '1',
-        title: 'Notification 1',
-        description: 'Description 1',
-        issuedBy: 'John Doe',
-        viewedBy: ['123'],
+        _id: "1",
+        title: "Notification 1",
+        description: "Description 1",
+        issuedBy: "John Doe",
+        viewedBy: ["123"],
       },
       {
-        _id: '2',
-        title: 'Notification 2',
-        description: 'Description 2',
-        issuedBy: 'Jane Smith',
-        viewedBy: ['123'],
+        _id: "2",
+        title: "Notification 2",
+        description: "Description 2",
+        issuedBy: "Jane Smith",
+        viewedBy: ["123"],
       },
     ];
-    const userJohn = { name: 'John', surname: 'Doe' };
-    const userJane = { name: 'Jane', surname: 'Smith' };
-  
+    const userJohn = { name: "John", surname: "Doe" };
+    const userJane = { name: "Jane", surname: "Smith" };
+
     Notification.find.mockResolvedValue(notifications);
-    jwt.verify.mockReturnValue({ userCode: '123' });
+    jwt.verify.mockReturnValue({ userCode: "123" });
     User.findOne.mockImplementation((query) => {
-      if (query.userCode === 'John Doe') {
-        return userJohn;
-      } else if (query.userCode === 'Jane Smith') {
-        return userJane;
+      if (query.userCode === "John Doe") {
+        return Promise.resolve(userJohn);
+      } else if (query.userCode === "Jane Smith") {
+        return Promise.resolve(userJane);
       }
+      return Promise.resolve(null);
     });
-  
+
     await notificationController.getNotifications(req, res);
-  
+
     expect(Notification.find).toHaveBeenCalled();
-    expect(jwt.verify).toHaveBeenCalledWith('token', process.env.JWT_SECRET);
-    expect(User.findOne).toHaveBeenCalledWith({ userCode: 'John Doe' });
-    expect(User.findOne).toHaveBeenCalledWith({ userCode: 'Jane Smith' });
+    expect(jwt.verify).toHaveBeenCalledWith("token", process.env.JWT_SECRET);
+    expect(User.findOne).toHaveBeenCalledWith({ userCode: "John Doe" });
+    expect(User.findOne).toHaveBeenCalledWith({ userCode: "Jane Smith" });
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({
       notifications: [
         {
-          _id: '1',
-          title: 'Notification 1',
-          description: 'Description 1',
-          issuedBy: 'John Doe',
-          viewedBy: ['123'],
+          _id: "1",
+          title: "Notification 1",
+          description: "Description 1",
+          issuedBy: "John Doe",
+          viewedBy: ["123"],
         },
         {
-          _id: '2',
-          title: 'Notification 2',
-          description: 'Description 2',
-          issuedBy: 'Jane Smith',
-          viewedBy: ['123'],
+          _id: "2",
+          title: "Notification 2",
+          description: "Description 2",
+          issuedBy: "Jane Smith",
+          viewedBy: ["123"],
         },
       ],
     });
   });
-  
-  it('should handle error when fetching notifications', async () => {
+
+  it("should handle error when fetching notifications", async () => {
     const req = {
       cookies: {
-        token: 'token',
+        token: "token",
       },
     };
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
-    const error = new Error('Error fetching notifications');
-  
+    const error = new Error("Error fetching notifications");
+
     Notification.find.mockRejectedValue(error);
-    
+
     try {
       await notificationController.getNotifications(req, res);
-    } catch (error) {  
+    } catch (error) {
       expect(Notification.find).toHaveBeenCalled();
       expect(console.error).toHaveBeenCalledWith(
-        'Error fetching notifications:',
+        "Error fetching notifications:",
         error
       );
       expect(res.status).toHaveBeenCalledWith(500);
       expect(res.json).toHaveBeenCalledWith({
-        error: 'Error fetching notifications',
+        error: "Error fetching notifications",
       });
     }
-  });  
+  });
 });
 
-describe('getUnreadNotifications', () => {
-  it('should fetch unread notifications successfully', async () => {
+describe("getUnreadNotifications", () => {
+  it("should fetch unread notifications successfully", async () => {
     const req = {
       cookies: {
-        token: 'token',
+        token: "token",
       },
     };
     const res = {
@@ -184,45 +185,45 @@ describe('getUnreadNotifications', () => {
     };
     const notifications = [
       {
-        _id: '1',
-        title: 'Notification 1',
-        description: 'Description 1',
+        _id: "1",
+        title: "Notification 1",
+        description: "Description 1",
         viewedBy: [],
       },
       {
-        _id: '2',
-        title: 'Notification 2',
-        description: 'Description 2',
-        viewedBy: ['456'],
+        _id: "2",
+        title: "Notification 2",
+        description: "Description 2",
+        viewedBy: ["456"],
       },
     ];
 
     Notification.find.mockResolvedValue(notifications);
-    jwt.verify.mockReturnValue({ userCode: '123' });
+    jwt.verify.mockReturnValue({ userCode: "123" });
 
     try {
       await notificationController.getUnreadNotifications(req, res);
     } catch (error) {
       expect(Notification.find).toHaveBeenCalledWith({
-        viewedBy: { $ne: '123' },
+        viewedBy: { $ne: "123" },
       });
-      expect(jwt.verify).toHaveBeenCalledWith('token', process.env.JWT_SECRET);
+      expect(jwt.verify).toHaveBeenCalledWith("token", process.env.JWT_SECRET);
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith({ unreadNotifications: 1 });
     }
   });
 
-  it('should handle error when fetching unread notifications', async () => {
+  it("should handle error when fetching unread notifications", async () => {
     const req = {
       cookies: {
-        token: 'token',
+        token: "token",
       },
     };
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
-    const error = new Error('Error fetching unread notifications');
+    const error = new Error("Error fetching unread notifications");
 
     Notification.find.mockRejectedValue(error);
 
@@ -231,17 +232,17 @@ describe('getUnreadNotifications', () => {
     } catch (error) {
       expect(Notification.find).toHaveBeenCalled();
       expect(console.error).toHaveBeenCalledWith(
-        'Error fetching unread notifications:',
+        "Error fetching unread notifications:",
         error
       );
       expect(res.status).toHaveBeenCalledWith(500);
       expect(res.json).toHaveBeenCalledWith({
-        error: 'Error fetching unread notifications',
+        error: "Error fetching unread notifications",
       });
     }
   });
 
-  it('should handle missing token', async () => {
+  it("should handle missing token", async () => {
     const req = {
       cookies: {},
     };
@@ -254,7 +255,7 @@ describe('getUnreadNotifications', () => {
 
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({
-      error: 'Error fetching unread notifications',
+      error: "Error fetching unread notifications",
     });
   });
 });
